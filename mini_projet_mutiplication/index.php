d1465e18bbe8cb5223d379d5dc3dfd568742c3f8
Manoah MPAH <manoah.mpah@lyc-jean-monnet-tours.ac-orleans-tours.fr>
2022-01-25 11:42:51 +0100

    startweb

---
+++ mini_projet_mutiplication/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="./style.css">
+    <title>Table de multiplication</title>
+</head>
+<body>
+    <section>
+        <div>
+            <div class="container">
+                <img id="img_first_page" src="./img/pexels-jeshootscom-442574.jpg" alt="">
+            </div>
+            <div id="texte_first_page">
+                <h2>Vous voulez apprendre <br> vos tables de multiplication</h2>
+                <a href="">Cliquer ici</a>
+            </div>
+        </div>
+    </section>
+
+    <section>
+    <form action="./" method="POST">
+        <label for="boutton">Choisit la table de multiplication, que tu veux apprendre
+            <br>1
+            <input id="boutton" type="radio" name="1">
+            2
+            <input id="boutton" type="radio" name="2">   
+            3
+            <input id="boutton" type="radio" name="3">            
+            4
+            <input id="boutton" type="radio" name="4">            
+            5
+            <input id="boutton" type="radio" name="5">            
+            6
+            <input id="boutton" type="radio" name="6">            
+            7
+            <input id="boutton" type="radio" name="7">            
+            8
+            <input id="boutton" type="radio" name="8">            
+            9
+            <input id="boutton" type="radio" name="9">            
+            10
+            <input id="boutton" type="radio" name="10">
+        </label>
+        <input type="submit" name="" id="">
+    </form>
+    </section>
+</body>
+</html>
