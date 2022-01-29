009b4451abadbd1fc6c9e33b3d811416a32b187a
Manoah <88939578+manoahmpah@users.noreply.github.com>
2022-01-29 09:35:30 +0100

    sauvegarde

--- mini_projet_mutiplication/exercice.php
+++ mini_projet_mutiplication/exercice.php
@@ -4,115 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>yooooooooo</title>
 </head>
-<body>
-<?php
-    if (isset($_POST['1']))
-{
-    echo '<label for="">
-    1x1 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x2 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x3 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x4 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x5 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x6 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x7 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x8 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    1x9 =
-    <input type="text">
-    </label>';
-    echo '<br> <br>';
-    echo '<label for="">
-    1x10 =
-    <input type="text">
-    </label>';
-}
-if (isset($_POST['2']))
-{
-    echo '<br><br>';
-    echo '<label for="">
-    2x1 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x2 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x3 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x4 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x5 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x6 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x7 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x8 =
-    <input type="text">
-    </label>';
-    echo '<br><br>';
-    echo '<label for="">
-    2x9 =
-    <input type="text">
-    </label>';
-    echo '<br> <br>';
-    echo '<label for="">
-    2x10 =
-    <input type="text">
-    </label>';
-}
-    ?>
-</body>
+    <body>
+        <h1>HELLO</h1>
+    <?php 
+    echo " world";
+?>
+    </body>
 </html>
--- mini_projet_mutiplication/index.php
+++ mini_projet_mutiplication/index.php
@@ -18,38 +18,70 @@
             </div>
             <div id="texte_first_page">
                 <h2>Vous voulez apprendre <br> vos tables de multiplication</h2>
-                <a href="">Cliquer ici</a>
+                <a href="#formulaire">Cliquer ici</a>
             </div>
         </div>
     </section>
 
-    <section class="size">
-        <h2></h2>
-    <form action="./exercice.php" method="POST">
-        <label for="boutton">
-            <br>1
-            <input id="boutton" type="checkbox" name="1">
-            2
-            <input id="boutton" type="checkbox" name="2">   
-            3
-            <input id="boutton" type="checkbox" name="3">            
-            4
-            <input id="boutton" type="checkbox" name="4">            
-            5
-            <input id="boutton" type="checkbox" name="5">            
-            6
-            <input id="boutton" type="checkbox" name="6">            
-            7
-            <input id="boutton" type="checkbox" name="7">            
-            8
-            <input id="boutton" type="checkbox" name="8">            
-            9
-            <input id="boutton" type="checkbox" name="9">            
-            10
-            <input id="boutton" type="checkbox" name="10">
-        </label>
-        <input type="submit" name="" id="">
-    </form>
+    <section id="formulaire" class="size">
+        <div id="place_formulaire">
+            <h2>POUR SAVOIR VOS BESOINS VEUILLIEZ REPONDRE A CES QUESTIONS :</h2>
+            <form action="./exercice.php" method="GET">
+                <label for="boutton">
+                    <legend>Quelles tables de multiplication veux tu apprendre :</legend>
+                    <br>
+                    1
+                    
+                    <input id="boutton" type="checkbox" name="1" value="nombre1">
+                    2
+                    <input id="boutton" type="checkbox" name="2">   
+                    3
+                    <input id="boutton" type="checkbox" name="3">            
+                    4
+                    <input id="boutton" type="checkbox" name="4">            
+                    5
+                    <input id="boutton" type="checkbox" name="5">            
+                    6
+                    <input id="boutton" type="checkbox" name="6">            
+                    7
+                    <input id="boutton" type="checkbox" name="7">            
+                    8
+                    <input id="boutton" type="checkbox" name="8">            
+                    9
+                    <input id="boutton" type="checkbox" name="9">            
+                    10
+                    <input id="boutton" type="checkbox" name="10">
+                </label>
+                <br>
+                <br>
+                <label for="booleen">
+                    Avez vous besoins de cours de maths 
+                    <input id="booleen" type="radio" value="OUI" name="booleen" required>
+                    OUI
+                    <input id="booleen" type="radio" value="OUI" name="booleen" required>
+                    NON
+                </label>
+                <br>
+                <br>
+                <label for="email">
+                    veulliez sasir votre email * :
+                    <input id="email" name="email" type="email" placeholder="mutiplication37@example.fr" required>
+                </label>
+                <br>
+                <br>
+                <input type="submit" name="" id="bouton">
+            </form>
+        </div>
+        <div>
+            <img id="img_section2" src="./img/pexels-koshevayak-4170628.jpg" alt="">
+        </div>
     </section>
+
+    <footer>
+        <div>
+            <h2>Contacter-nous</h2>
+
+        </div>
+    </footer>
 </body>
 </html>
